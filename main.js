807e30b26c03efb4a7465d8d52e0254ce2bcc06e
Austin Flatt <austindflatt@gmail.com>
2021-10-20 12:56:51 -0400

    update of problems finished

--- main.js
+++ main.js
@@ -6,22 +6,42 @@ function isEvenlyDivisible(number, number2) {
   }
 }
 
-function halfSquare() {
+function halfSquare(number) {
+  number = number ** 2 / 2;
+  return number
 }
 
-function isLong() {
+function isLong(str) {
+  if (str.length >= 15) {
+  return true
+  } else {
+    return false
+  }
 }
 
-function exclaim() {
+// Come back to finish, not done yet
+function exclaim(str) {
+  let newStr = str;
+  while (newStr[newStr.length - 1] === "!") {
+		newStr = newStr.slice(0, -1);
+	}
+	return newStr + "!";
 }
 
-function countWords() {
+function countWords(str) {
+  let word = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      word++;
+    }
+  }
+  return word;
 }
 
 function containsDigit() {
 }
 
-function containsLowerCase() {
+function containsLowerCase(str) {
 }
 
 function containsUpperCase() {
@@ -36,7 +56,11 @@ function containsSpace() {
 function digits() {
 }
 
-function truncate() {
+function truncate(str) {
+  if (str.length <= 8) {
+	return str;
+	}
+	return str.slice(0, 8) + "...";
 }
 
 function isValidPassword() {
